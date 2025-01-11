5bbdd7f481103a6545048e46ddf748e97f891b88
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-01-12 03:30:54 +0900

    ♻️ Refactor: 중복코드 제거

--- Midnight-Snacker/src/main/java/com/example/Midnight/Snacker/security/principal/PrincipalDetails.java
+++ Midnight-Snacker/src/main/java/com/example/Midnight/Snacker/security/principal/PrincipalDetails.java
@@ -35,10 +35,6 @@ public class PrincipalDetails implements UserDetails {
         return member.getNickname();
     }
 
-    public Long getId() {
-        return member.getId();
-    }
-
     @Override
     public boolean isAccountNonExpired() {
         return true;
