c4258f18cca57b53ad5275756ba27e6c33a3a939
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-01-12 00:12:07 +0900

    Update MidnightSnackerApplicationTests.java

--- Midnight-Snacker/src/test/java/com/example/Midnight/Snacker/MidnightSnackerApplicationTests.java
+++ Midnight-Snacker/src/test/java/com/example/Midnight/Snacker/MidnightSnackerApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.Midnight.Snacker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MidnightSnackerApplicationTests.class)
 class MidnightSnackerApplicationTests {
 
 	@Test
