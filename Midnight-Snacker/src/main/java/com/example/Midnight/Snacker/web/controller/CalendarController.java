155e5c1c7f42080846c5e63e26bb80c79028f351
seoyeon0103 <angel6462@naver.com>
2025-01-12 05:40:15 +0900

    충돌 고침

--- Midnight-Snacker/src/main/java/com/example/Midnight/Snacker/web/controller/CalendarController.java
+++ Midnight-Snacker/src/main/java/com/example/Midnight/Snacker/web/controller/CalendarController.java
@@ -32,10 +32,11 @@ public class CalendarController {
             @Parameter(name = "user", hidden = true) @AuthUser Member member,
             @RequestParam(value = "category")Category category,
             @RequestParam(value = "colorType")Color color,
-            @RequestPart("request")RegisterRequestDTO request,
+            @RequestParam(value = "date")LocalDateTime date,
+            @RequestParam(value = "detailFood") String detailFood,
             @RequestPart(value = "image") MultipartFile image){
         Long calendarId = calendarService.addRecord(member, category,
-                color, image, request);
+                color, image, date, detailFood);
 
         return ApiResponse.of(SuccessStatus.ADD_CALENDAR_OK, calendarId);
     }
