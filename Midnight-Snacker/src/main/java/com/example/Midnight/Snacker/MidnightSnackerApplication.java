5a14d5381c3cc2ddbfebf9297ab702b99f26aaab
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-01-11 15:41:23 +0900

    🎉 chore: 스프링 부트 세팅

---
+++ Midnight-Snacker/src/main/java/com/example/Midnight/Snacker/MidnightSnackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Midnight.Snacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidnightSnackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidnightSnackerApplication.class, args);
+	}
+
+}
---
+++ Midnight-Snacker/src/test/java/com/example/Midnight/Snacker/MidnightSnackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Midnight.Snacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidnightSnackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
