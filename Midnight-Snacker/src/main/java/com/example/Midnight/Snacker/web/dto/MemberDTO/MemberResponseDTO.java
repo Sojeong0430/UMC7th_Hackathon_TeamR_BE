4db5fa05edec39933502f29f3c15cf4197a9ef71
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-01-11 22:07:32 +0900

    ✨ Feat: DTO 설정

---
+++ Midnight-Snacker/src/main/java/com/example/Midnight/Snacker/web/dto/MemberDTO/MemberResponseDTO.java
@@ -0,0 +1,14 @@
+package com.example.Midnight.Snacker.web.dto.MemberDTO;
+
+import lombok.*;
+
+public class MemberResponseDTO {
+    @Getter
+    @Builder
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class MyPageResponse {
+        Long memberId;
+        String nickname;
+    }
+}
